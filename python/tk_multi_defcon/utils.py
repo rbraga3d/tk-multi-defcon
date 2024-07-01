167d86492531b363442e5068f3be50b8617ca672
Rafael Braga <67921252+rbraga3d@users.noreply.github.com>
2024-07-01 15:34:52 -0300

    Adicionado novo fileImagePath template usando os tokens do shotgrid

---
+++ python/tk_multi_defcon/utils.py
@@ -0,0 +1,46 @@
+import maya.cmds as cmds
+import sgtk
+
+
+def resolve_image_file_prefix(engine, image_file_prefix):
+    context = engine.context
+
+    keys = {
+        "Project": sgtk.StringKey("Project"),
+        "Step": sgtk.StringKey("Step"),
+        "Sequence": sgtk.StringKey("Sequence"),
+        "Shot": sgtk.StringKey("Shot"),
+        "Asset": sgtk.StringKey("Asset"),
+        "sg_asset_type": sgtk.StringKey("sg_asset_type"),
+        "version": sgtk.IntegerKey("version", format_spec="03")
+    }
+
+
+    work_file_path = cmds.file(query=True, sn=True)
+    work_file_norm_path = sgtk.util.ShotgunPath.normalize(work_file_path)
+    work_template_path = engine.sgtk.template_from_path(work_file_norm_path)
+    work_file_fields = work_template_path.get_fields(work_file_norm_path)
+
+
+    fields = {
+        "Project": context.project.get('name'),
+        "Step": context.step.get("name"),
+        "Sequence": work_file_fields.get("Sequence"),
+        "Shot": work_file_fields.get("Shot"),
+        "Asset": work_file_fields.get("Asset", ""),
+        "sg_asset_type": work_file_fields.get("sg_asset_type"),
+        "version": work_file_fields.get("version")
+
+    }
+
+    if context.entity["type"] == "Asset":
+        fields["Shot"] = fields["Asset"]
+
+
+    template = sgtk.TemplateString(image_file_prefix, keys)
+
+    resolved_image_file_prefix = template.apply_fields(fields)
+    return resolved_image_file_prefix
+
+
+    
